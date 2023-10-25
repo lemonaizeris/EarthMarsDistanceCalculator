12f739c21500c5ab29101c9cd749a029f77fde7d
Laimonas Suchockas <laimonas.suchockas@gmail.com>
2023-10-25 19:46:40 +0300

    Plotted Mars barycenter

--- main.py
+++ main.py
@@ -30,52 +30,53 @@ def main():
     solar_system_ephemeris.set("jpl")
 
     positions_earth, lightTimes_earth = spiceypy.spkezr('Earth', time_spice, 'ECLIPJ2000', 'NONE', 'Sun')
-    positions_mars, lightTimes_mars = spiceypy.spkezr('Mars', time_spice, 'ECLIPJ2000', 'NONE', 'Sun')
+    positions_mars, lightTimes_mars = spiceypy.spkezr('Mars Barycenter', time_spice, 'ECLIPJ2000', 'NONE', 'Sun')
 
     per_earth = np.array(positions_earth)[:, :3] * u.km
     x_earth = per_earth[:, 0].to(u.au)
     y_earth = per_earth[:, 1].to(u.au)
     z_earth = per_earth[:, 2].to(u.au)
 
-    print(x_earth)
-    print(y_earth)
-    print(z_earth)
-
     per_mars = np.array(positions_mars)[:, :3] * u.km
     x_mars = per_mars[:, 0].to(u.au)
     y_mars = per_mars[:, 1].to(u.au)
     z_mars = per_mars[:, 2].to(u.au)
 
-    fig, ax = plt.subplots(1, 3, sharex=True, sharey=True, figsize=(15, 5))
-    ax[0].plot(x_earth, z_earth)
-    ax[0].set_xlabel('x (AU)')
-    ax[0].set_ylabel('z (AU)')
+    print(x_mars)
+    print(y_mars)
+    print(z_mars)
 
-    ax[1].plot(x_earth, y_earth)
-    ax[1].set_xlabel('x (AU)')
-    ax[1].set_ylabel('y (AU)')
+    fig, ax = plt.subplots(2, 3, sharex=True, sharey=True, figsize=(15, 10))
+    ax[0,0].plot(x_earth, z_earth)
+    ax[0,0].set_xlabel('x (AU)')
+    ax[0,0].set_ylabel('z (AU)')
 
-    ax[2].plot(y_earth, z_earth)
-    ax[2].set_xlabel('y (AU)')
-    ax[2].set_ylabel('z (AU)')
+    ax[0,1].plot(x_earth, y_earth)
+    ax[0,1].set_xlabel('x (AU)')
+    ax[0,1].set_ylabel('y (AU)')
 
-    ax[3].plot(x_mars, z_mars)
-    ax[3].set_xlabel('x (AU)')
-    ax[3].set_ylabel('z (AU)')
+    ax[0,2].plot(y_earth, z_earth)
+    ax[0,2].set_xlabel('y (AU)')
+    ax[0,2].set_ylabel('z (AU)')
 
-    ax[4].plot(x_mars, y_mars)
-    ax[4].set_xlabel('x (AU)')
-    ax[4].set_ylabel('y (AU)')
+    ax[1,0].plot(x_mars, z_mars)
+    ax[1,0].set_xlabel('x (AU)')
+    ax[1,0].set_ylabel('z (AU)')
 
-    ax[5].plot(y_mars, z_mars)
-    ax[5].set_xlabel('y (AU)')
-    ax[5].set_ylabel('z (AU)')
+    ax[1,1].plot(x_mars, y_mars)
+    ax[1,1].set_xlabel('x (AU)')
+    ax[1,1].set_ylabel('y (AU)')
+
+    ax[1,2].plot(y_mars, z_mars)
+    ax[1,2].set_xlabel('y (AU)')
+    ax[1,2].set_ylabel('z (AU)')
 
 
-    for a in ax:
-        a.grid()
-        a.set_ylim(-1, 1)
-        a.set_xlim(-1, 1)
+    for b in ax:
+        for a in b:
+            a.grid()
+            a.set_ylim(-2, 2)
+            a.set_xlim(-2, 2)
 
     plt.tight_layout()
     plt.show()
